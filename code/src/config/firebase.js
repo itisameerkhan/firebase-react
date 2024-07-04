6d108929416cb1a7549efc0db8330f3d624522e7
itisameerkhan <itisameerkhan@gmail.com>
2024-07-04 12:16:52 +0530

    firebase config

--- code/src/config/firebase.js
+++ code/src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvLoSGdHvB_UqfghWqyZsuOk-aL2ovQdA",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "fir-tuts-1-babf8.firebaseapp.com",
   projectId: "fir-tuts-1-babf8",
   storageBucket: "fir-tuts-1-babf8.appspot.com",
   messagingSenderId: "977506235538",
   appId: "1:977506235538:web:b50c1c94844669c245e24b",
-  measurementId: "G-E8YLZK2ECL"
+  measurementId: "G-E8YLZK2ECL",
 };
 
 const app = initializeApp(firebaseConfig);
